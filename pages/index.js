1cfc93672dd1bfdb24745d8ad7113f42a0e40c81
Neil McKay <neil.d.mckay@googlemail.com>
2021-05-03 23:23:56 +0100

    Fixing Vercel

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,16 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import Container from "@/components/container";
-import { fetchAPI } from "@/lib/strapi/api";
+import React from "react";
 import Posts from "@/components/posts";
+import Container from "@/components/container";
+import Seo from "@/components/seo";
+import { fetchAPI } from "@/lib/strapi/api";
 
 const Home = ({ articles, categories, homepage }) => {
   return (
     <Container categories={categories}>
+      <Seo seo={homepage.seo} />
       <div>
-        <div className="container">
-          <h1>{homepage.hero.title}</h1>
+        <div className="homepage__container">
+          <h1 className="homepage__title">{homepage.hero.title}</h1>
           <Posts articles={articles} />
         </div>
       </div>
@@ -18,11 +19,13 @@ const Home = ({ articles, categories, homepage }) => {
 };
 
 export async function getStaticProps() {
+  // Run API calls in parallel
   const [articles, categories, homepage] = await Promise.all([
     fetchAPI("/articles?status=published"),
     fetchAPI("/categories"),
     fetchAPI("/homepage"),
   ]);
+
   return {
     props: { articles, categories, homepage },
     revalidate: 1,
